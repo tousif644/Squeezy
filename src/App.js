8e933ed537336fa651f142f2527bce27cb39e220
tousif644 <tousif2567@gmail.com>
2023-05-25 20:22:11 +0600

    Navbar two links added and dark mode to light switch added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import "./style.css"
 function App() {
   return (
     <ChakraProvider>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,22 +10,51 @@ import {
   Stack,
   useColorModeValue,
   useColorMode,
+  Icon,
 } from "@chakra-ui/react";
 import { Link as ReactLink } from "react-router-dom";
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 
+// array of links
+const links = [
+  {
+    linkname: "Products",
+    path: "/products",
+  },
+  {
+    linkname: "Shopping Cart",
+    path: "/cart",
+  },
+];
+
+// NavLink
+const NavLink = ({ path, children }) => (
+  <Link
+    as={ReactLink}
+    to={path}
+    px={2}
+    py={2}
+    rounded="md"
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+  >
+    {children}
+  </Link>
+);
+
 function Navbar() {
   const { isOpen, onClose, onOpen } = useDisclosure();
-  const { colormode, toggleColoMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Box
-      boxShadow="md"
+      boxShadow="lg"
       p="2"
-      rounded="md"
-      bg="white"
+      bg={useColorModeValue("whitesmoke", "gray.400")}
       px={4}
     >
-      <Flex h={16} alignItems="center" justifyConten="space-between">
+      <Flex h={16} alignItems="center" justifyContent="space-between">
         <IconButton
           size="md"
           icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
@@ -35,12 +64,28 @@ function Navbar() {
         <HStack>
           <Link as={ReactLink} to="/">
             <Flex alignItems="center">
-              <Text fontWeight={600} fontSize={36} fontFamily={"Poppins"}>
+              <Text fontWeight={600} fontSize={36}>
                 Squeezy.
               </Text>
             </Flex>
           </Link>
+          <HStack>
+            {links.map((link) => (
+              <NavLink key={link.linkname} path={link.path}>
+                {link.linkname}
+              </NavLink>
+            ))}
+          </HStack>
         </HStack>
+        <Flex alignItems="center">
+          <NavLink>
+            <Icon
+              as={colorMode === "light" ? MoonIcon : SunIcon}
+              alignSelf={"center"}
+              onClick={() => toggleColorMode()}
+            />
+          </NavLink>
+        </Flex>
       </Flex>
     </Box>
   );
